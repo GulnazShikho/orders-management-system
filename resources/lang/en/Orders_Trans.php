41d242f5489c0d8d144105549b7319c855b6649f
Gulnaz Shikho <gulnazshikho@gmail.com>
2023-08-27 09:23:21 -0500

    download theme and add localization

---
+++ resources/lang/en/Orders_Trans.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+
+    'title_page' => 'Orders',
+    'List_Order' => 'Orders List',
+    'add_Order' => 'Add Order',
+    'edit_Order'=> 'Edit Order',
+    'delete_Order'=> 'Delete Order',
+    'Warning_Order'=> 'Are You Sure Of The Deleting Process ?',
+    'Order_Title' => 'Order Title',
+    'Order' => 'Order',
+    'Notes' => 'Notes',
+    'submit' => 'submit',
+    'Name'=>'Name',
+    'Processes'=>'Processes',
+    'Edit'=>'Edit',
+    'exists'=>'This Order already exists',
+    'Delete'=>'Delete',
+    'Close' => 'Close',
+    'Status' => 'Order Status',
+    'Choose' => 'Choose',
+
+];
---
+++ resources/lang/en/main_trans.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+
+
+    'Dashboard'=>'Dashboard',
+    'Orders' => 'Orders',
+    'Programname' =>'Orders MAnagement System',
+    'OrdersList'=>'Orders List',
+];
